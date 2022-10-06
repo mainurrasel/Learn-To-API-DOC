2dc30097f9c0c9a4df4c6523814c3214d616b4e4
mainurrasel <mainurrrasel@gmail.com>
2022-10-06 17:15:17 +0600

    Added response example

--- Swagger/Swagger-hapi/index.js
+++ Swagger/Swagger-hapi/index.js
@@ -11,8 +11,24 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 const payload_scheme = Joi.object({
-    author: Joi.string().min(3).max(30).default('Rasel').required(),
-    title: Joi.string().min(3).max(30).default('Rasel').required(),
+    author: Joi.string().min(3).max(30).required(),
+    title: Joi.string().min(3).max(30).required(),
+});
+
+const param_scheme = Joi.object({
+    id: Joi.string().required()
+});
+
+const query_param_scheme = Joi.object({
+    id: Joi.string().required()
+});
+
+var responseModel = Joi.object({
+    author: Joi.string(),
+    title: Joi.string(),
+    id: Joi.string()
+}).meta({
+  className: 'Result'
 });
 
 (async () => {
@@ -21,6 +37,7 @@ const payload_scheme = Joi.object({
         port: 3000,
     });
 
+//specify our documentation and the version number
     const swaggerOptions = {
         info: {
             title: 'Books API Documentation',
@@ -28,6 +45,7 @@ const payload_scheme = Joi.object({
         },
     };
 
+//register (before the server starts) all plugins and Swagger configuration
     await server.register([
         Inert,
         Vision,
@@ -44,6 +62,7 @@ const payload_scheme = Joi.object({
         console.log(err);
     }
 
+//Adding description, tags, notes to API routes
     server.route({
         method: 'GET',
         path: '/',
@@ -52,6 +71,7 @@ const payload_scheme = Joi.object({
         }
     });
 
+//Adding description, tags, notes to API routes
     server.route({
         method: 'GET',
         path: '/book-list',
@@ -66,6 +86,25 @@ const payload_scheme = Joi.object({
         },
     });
 
+//Adding description, tags, notes to API routes with query param
+    server.route({
+        method: 'GET',
+        path: '/book-list-query',
+        options: {
+            description: 'Get book list',
+            notes: 'Returns an array of books',
+            tags: ['api'],
+            validate: {
+                query: query_param_scheme,
+            },
+        },
+        handler: async (request, h) => {
+            let books = await readFile('./books.json', 'utf8');
+            return h.response(JSON.parse(books));
+        },
+    });
+
+//Adding description, tags, notes to API routes
     server.route({
         method: 'POST',
         path: '/add-book',
@@ -79,6 +118,7 @@ const payload_scheme = Joi.object({
                     return h.response({ code: 301, status: false, message: err?.message }).takeover();
                 },
             },
+            // response: {schema: responseModel}
         },
         handler: async (request, h) => {
             let book = request.payload;
@@ -91,6 +131,7 @@ const payload_scheme = Joi.object({
         },
     });
 
+//Adding description, tags, notes to API routes
     server.route({
         method: 'PUT',
         path: '/update-book-list/{id}',
@@ -99,11 +140,9 @@ const payload_scheme = Joi.object({
             notes: 'Returns an array of books',
             tags: ['api'],
             validate: {
-                params: Joi.object({
-                     id: Joi.string().required()
-                 }),
-                 payload: payload_scheme,
-             },
+                params: param_scheme,
+                payload: payload_scheme,
+            },
         },
         handler: async (request, h) => {
             let updBook = request.payload;
@@ -121,6 +160,7 @@ const payload_scheme = Joi.object({
         },
     });
 
+//Adding description, tags, notes to API routes
     server.route({
         method: 'DELETE',
         path: '/delete-book/{id}',
@@ -129,9 +169,7 @@ const payload_scheme = Joi.object({
             notes: 'Returns an array of books',
             tags: ['api'],
             validate: {
-               params: Joi.object({
-                    id: Joi.string().required()
-                }),
+                params: param_scheme,
             },
         },
         handler: async (request, h) => {
